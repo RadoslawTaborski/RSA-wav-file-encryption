e4ae5ca3056ab6221890aee42b5ddd6c85fd6edd
Radosław Taborski <radoslaw.taborski@gmail.com>
2016-11-16 00:45:50 +0100

    działa

---
+++ wav2/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Collections.ObjectModel;
+using AForge.Math;
+using System.Windows.Controls.DataVisualization;
+using Microsoft.Win32;
+
+namespace wav2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public WavFile wav;
+        public ObservableCollection<DataPoint> Points { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MinWidth = 550;
+            this.MinHeight = 488;
+            dataGrid.Items.Add(new { Name = "ChrunkID", Value = "" });
+            dataGrid.Items.Add(new { Name = "ChrunkSize", Value = "" });
+            dataGrid.Items.Add(new { Name = "Format", Value = "" });
+            dataGrid.Items.Add(new { Name = "Subchunk1ID", Value = "" });
+            dataGrid.Items.Add(new { Name = "Subchunk1Size", Value = "" });
+            dataGrid.Items.Add(new { Name = "AudioFormat", Value = "" });
+            dataGrid.Items.Add(new { Name = "NumChanels", Value = "" });
+            dataGrid.Items.Add(new { Name = "SampleRate", Value = "" });
+            dataGrid.Items.Add(new { Name = "ByteRate", Value = "" });
+            dataGrid.Items.Add(new { Name = "BlockAlign", Value = "" });
+            dataGrid.Items.Add(new { Name = "BitsPerSample", Value = "" });
+            dataGrid.Items.Add(new { Name = "Subchunk2ID", Value = "" });
+            dataGrid.Items.Add(new { Name = "Subchunk2Size", Value = "" });
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog okienko = new OpenFileDialog();
+            okienko.Filter = "Pliki (wav)|*.wav";
+            if (okienko.ShowDialog() == true)
+            {
+                Chart1.Series.Clear();
+                Chart1.Axes.Clear();
+
+                tbPath.Text=okienko.FileName;
+                Points = new ObservableCollection<DataPoint>();
+                byte[] bytes = System.IO.File.ReadAllBytes(okienko.FileName);
+                wav = new WavFile(bytes);
+
+                dataGrid.Items.Clear();
+                dataGrid.Items.Add(new { Name = "ChrunkID", Value = wav.ChrunkID });
+                dataGrid.Items.Add(new { Name = "ChrunkSize", Value = wav.ChrunkSize });
+                dataGrid.Items.Add(new { Name = "Format", Value = wav.Format });
+                dataGrid.Items.Add(new { Name = "Subchunk1ID", Value = wav.Subchunk1ID });
+                dataGrid.Items.Add(new { Name = "Subchunk1Size", Value = wav.Subchunk1Size });
+                dataGrid.Items.Add(new { Name = "AudioFormat", Value = wav.AudioFormat });
+                dataGrid.Items.Add(new { Name = "NumChanels", Value = wav.NumChanels });
+                dataGrid.Items.Add(new { Name = "SampleRate", Value = wav.SampleRate });
+                dataGrid.Items.Add(new { Name = "ByteRate", Value = wav.ByteRate });
+                dataGrid.Items.Add(new { Name = "BlockAlign", Value = wav.BlockAlign });
+                dataGrid.Items.Add(new { Name = "BitsPerSample", Value = wav.BitsPerSample });
+                dataGrid.Items.Add(new { Name = "Subchunk2ID", Value = wav.Subchunk2ID });
+                dataGrid.Items.Add(new { Name = "Subchunk2Size", Value = wav.Subchunk2Size });
+
+                var tabCom = new Complex[1024];
+                for (int i = 0; i < 1024; i++)
+                {
+                    tabCom[i] = new Complex(wav.Data[0, i], 0);
+                }
+
+                FourierTransform.FFT(tabCom, FourierTransform.Direction.Forward);
+
+                for (int i = 0; i < 512; i++)
+                {
+                    Points.Add(new DataPoint() { X = (wav.SampleRate * i) / 511, Y = tabCom[i].Magnitude * 1000 });
+                }
+
+                var style = new Style(typeof(Polyline));
+                style.Setters.Add(new Setter(Polyline.StrokeThicknessProperty, 1d));
+
+                var pointStyle = new Style(typeof(LineDataPoint));
+                pointStyle.Setters.Add(new Setter(LineDataPoint.TemplateProperty, null));
+
+                //var axisStyle = new Style(typeof(NumericAxisLabel));
+                // pointStyle.Setters.Add(new Setter(NumericAxisLabel.StringFormatProperty, "{0:0.00000}"));
+
+                var HideLegendStyle = new Style(typeof(Legend));
+                HideLegendStyle.Setters.Add(new Setter(Legend.WidthProperty, 0.0));
+                HideLegendStyle.Setters.Add(new Setter(Legend.HeightProperty, 0.0));
+                HideLegendStyle.Setters.Add(new Setter(Legend.VisibilityProperty, Visibility.Collapsed));
+
+                var series = new LineSeries
+                {
+                    PolylineStyle = style,
+                    ItemsSource = Points,
+                    DependentValuePath = "Y",
+                    IndependentValuePath = "X",
+                    DataPointStyle = pointStyle,
+                    LegendItemStyle = null,
+                };
+
+                var axisY = new LinearAxis { Orientation = AxisOrientation.Y, Title = "Amplitude", ShowGridLines = true, };
+                var axisX = new LinearAxis { Orientation = AxisOrientation.X, Title = "Frequence[Hz]", ShowGridLines = true, };
+
+                Chart1.Series.Add(series);
+                Chart1.Axes.Add(axisX);
+                Chart1.Axes.Add(axisY);
+                Chart1.LegendStyle = HideLegendStyle;
+
+            }
+        }
+    }
+
+    public class DataPoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    } // class
+
+}
---
+++ wav2/WavFile.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace wav2
+{
+    public class WavFile
+    {
+        public string ChrunkID { get; private set; }
+        public int ChrunkSize { get; private set; }
+        public string Format { get; private set; }
+        public string Subchunk1ID { get; private set; }
+        public int Subchunk1Size { get; private set; }
+        public int AudioFormat { get; private set; }
+        public int NumChanels { get; private set; }
+        public int SampleRate { get; private set; }
+        public int ByteRate { get; private set; }
+        public int BlockAlign { get; private set; }
+        public int BitsPerSample { get; private set; }
+        public string Subchunk2ID { get; private set; }
+        public int Subchunk2Size { get; private set; }
+        public float[,] Data { get; private set; }
+
+        public WavFile(byte[] bytes)
+        {
+            ChrunkID = readString(bytes, 0, 4);
+            ChrunkSize = readValue(bytes, 4, 8);
+            Format = readString(bytes, 8, 12);
+            Subchunk1ID = readString(bytes, 12, 16);
+            Subchunk1Size = readValue(bytes, 16, 20);
+            AudioFormat = readValue(bytes, 20, 22);
+            NumChanels = readValue(bytes, 22, 24);
+            SampleRate = readValue(bytes, 24, 28);
+            ByteRate = readValue(bytes, 28, 32);
+            BlockAlign = readValue(bytes, 32, 34);
+            BitsPerSample = readValue(bytes, 34, 36);
+            Subchunk2ID = readString(bytes, 36, 40);
+            Subchunk2Size = readValue(bytes, 40, 44);
+            Data = new float[NumChanels, Subchunk2Size / 2 / NumChanels];
+            int index = 0;
+            int nr = 44;
+            while (nr < Subchunk2Size + 42)
+            {
+                for (var i = 0; i < NumChanels; ++i)
+                {
+                    Data[i, index] = readFloat2(bytes, nr, nr + 2);
+                    nr += 2;
+                    if (nr >= Subchunk2Size + 42)
+                        break;
+                    // Console.WriteLine(Data[i, index]);
+                }
+                index++;
+            }
+        }
+
+        private static Int16 getSample(byte[] buffer, int position)
+        {
+            return (Int16)(((buffer[position + 1] & 0xff) << 8) | (buffer[position] & 0xff));
+        }
+
+        private float readFloat(byte[] bytes, int start)
+        {
+            return (float)getSample(bytes, start) / (float)Int16.MaxValue;
+        }
+
+        private float readFloat2(byte[] bytes, int start, int end)
+        {
+            Int16 result = 0;
+            int index = 0;
+            var hexArray = new string[end - start];
+            for (int i = start; i < end; i++)
+            {
+                hexArray[index] = bytes[i].ToString("X2");
+                index++;
+            }
+
+            string sizeStr = "";
+            foreach (var str in hexArray)
+            {
+                sizeStr = str + sizeStr;
+            }
+            result = Convert.ToInt16(sizeStr, 16);
+
+            float f = ((float)result) / (float)Int16.MaxValue;
+            if (f > 1) f = 1;
+            if (f < -1) f = -1;
+
+            return f;
+        }
+
+        private string readString(byte[] bytes, int start, int end)
+        {
+            string result = "";
+            for (int i = start; i < end; i++)
+            {
+                result = result + Convert.ToChar(bytes[i]);
+            }
+            return result;
+        }
+
+        private int readValue(byte[] bytes, int start, int end)
+        {
+            int result = 0;
+            int index = 0;
+            var hexArray = new string[end - start];
+            for (int i = start; i < end; i++)
+            {
+                hexArray[index] = bytes[i].ToString("X2");
+                index++;
+            }
+
+            string sizeStr = "";
+            foreach (var str in hexArray)
+            {
+                sizeStr = str + sizeStr;
+            }
+            result = Convert.ToInt32(sizeStr, 16);
+
+            return result;
+        }
+
+        public void Header()
+        {
+            Console.WriteLine("ChrunkID: " + ChrunkID);
+            Console.WriteLine("ChrunkSize: " + ChrunkSize.ToString());
+            Console.WriteLine("Format: " + Format);
+            Console.WriteLine("Subchunk1ID: " + Subchunk1ID);
+            Console.WriteLine("Subchunk1Size: " + Subchunk1Size.ToString());
+            Console.WriteLine("AudioFormat: " + AudioFormat.ToString());
+            Console.WriteLine("NumChanels: " + NumChanels.ToString());
+            Console.WriteLine("SampleRate: " + SampleRate.ToString());
+            Console.WriteLine("ByteRate: " + ByteRate.ToString());
+            Console.WriteLine("BlockAlign: " + BlockAlign.ToString());
+            Console.WriteLine("BitsPerSample: " + BitsPerSample.ToString());
+            Console.WriteLine("Subchunk2ID: " + Subchunk2ID);
+            Console.WriteLine("Subchunk2Size: " + Subchunk2Size.ToString());
+
+            /*for (var j = 0; j < Subchunk2Size / 2 / NumChanels; ++j)
+                for (var i = 0; i < NumChanels; ++i)
+                    Console.WriteLine(Data[i, j].ToString() + " ");*/
+
+            /* for (var j = 0; j < Subchunk2Size / 2 / NumChanels; ++j)
+                 Console.WriteLine(Data[0, j].ToString() + " ");
+             Console.WriteLine("end");*/
+        }
+    }
+}
